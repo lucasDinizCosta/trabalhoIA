038887715d2f9c9b4272ac27b4642e76c417f4c2
LucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2019-06-23 15:39:10 -0300

    Busca em profundidade limitada

--- geradorLabirintoJavascript/maze.js
+++ geradorLabirintoJavascript/maze.js
@@ -131,7 +131,7 @@ Maze.prototype.removeWalls = function (a, b) {
             *       Algoritmos de busca     *
             *********************************/
 
-function heuristica(xInicial, yInicial, xFinal, yFinal){
+Maze.prototype.heuristica = function (xInicial, yInicial, xFinal, yFinal){
   if(Math.abs(xFinal - xInicial)  <  Math.abs(yFinal - yInicial))
     return Number(Math.abs(xInicial - xFinal) + Math.abs((yInicial - yFinal) - Math.abs(xInicial - xFinal)));
   else
@@ -142,15 +142,15 @@ function heuristica(xInicial, yInicial, xFinal, yFinal){
     return Number(Math.abs(xInicial - xFinal));
 }
 
-function backtracking(verticeInicial, verticeObjetivo){
+Maze.prototype.backtracking = function(verticeInicial, verticeObjetivo){
   var tempoInicial = performance.now();
 
   console.log("Executando backtracking");
   var pilhaBT = [];
   var visitados = [];//[linhas][cols];            //Matriz de visitados
-  for (var i = 0; i < linhas; i++) {
+  for (var i = 0; i < this.linhas; i++) {
     visitados[i] = [];
-    for(var j = 0; j < cols; j++){
+    for(var j = 0; j < this.colunas; j++){
       visitados[i][j] = false;
     }
   }
@@ -166,7 +166,7 @@ function backtracking(verticeInicial, verticeObjetivo){
     for (var i = 0; i < s.wall.length; i++) {
       if(n.wall[i] === false){                                      //Checa se não há parede
         //console.log(i + " ---- "+ n.i + " ------(n) BACKTRACKING ---- " + n.j);
-        var auxVizinho = n.getVizinho(i, matriz);                   //Captura a celula do vizinho na matriz
+        var auxVizinho = n.getVizinho(i, this.matriz);                   //Captura a celula do vizinho na matriz
         //console.log(auxVizinho.i + " ------ BACKTRACKING ---- " + auxVizinho.j);
         if(visitados[auxVizinho.i][auxVizinho.j] === false){        //vizinho não visitado
           n = auxVizinho;                                           //Atualiza para o próximo vértice
@@ -210,8 +210,8 @@ function backtracking(verticeInicial, verticeObjetivo){
       console.log(texto);
       var numVisitados = 0;
       texto = "\n\nQuem foi visitado: ";
-      for (var i = 0; i < linhas; i++) {
-        for(var j = 0; j < cols; j++){
+      for (var i = 0; i < this.linhas; i++) {
+        for(var j = 0; j < this.colunas; j++){
           if(visitados[i][j]){
               texto = texto + "Celula["+i+"]["+j+"] -- ";
               numVisitados++;
@@ -221,7 +221,135 @@ function backtracking(verticeInicial, verticeObjetivo){
       console.log(texto);
       console.log("\nQuantidade total de vértices visitados: " + numVisitados);
       var tempoFinal = performance.now();
-      console.log("\nTempo de execução: " + parseFloat((tempoFinal - tempoInicial)).toFixed(3));    ///Mede o tempo somente na função
+      console.log("\nTempo de execução: " + parseFloat((tempoFinal - tempoInicial)).toFixed(3) + " milissegundos");    ///Mede o tempo somente na função
     }
   }
+}
+/**
+
+Estratégia de controle: Inserir na pilha de modo que sejam exacutadas a ordem
+do vetor de paredes, ou seja, cima, direita, baixo e esquerda
+*/
+
+Maze.prototype.buscaProfundidadeLimitada = function (verticeInicial, verticeObjetivo, limiteProfMax){
+  var tempoInicial = performance.now();
+
+  console.log("Executando busca em profundidade limitada: ");
+  //var pilhaBT = [];
+  var abertos = [];
+  var fechados = [];
+  var solucao = [];
+  var visitados = [];//[linhas][cols];            //Matriz de visitados
+  for (var i = 0; i < this.linhas; i++) {
+    visitados[i] = [];
+    for(var j = 0; j < this.colunas; j++){
+      visitados[i][j] = false;
+    }
+  }
+  var limiteProfundidade = 0;             ///Controla a profundidade visando não ocorrer a possibilidade de "caminhos infinitos" ou muito grandes, o que seria inviável
+  var fim = this.matriz[verticeObjetivo[0]][verticeObjetivo[1]];
+  var s = this.matriz[verticeInicial[0]][verticeInicial[1]];
+  abertos.push(s);
+  limiteProfundidade++;
+  visitados[s.i][s.j] = true;                                       //Visitando o primeiro vertice
+  var fracasso = false, sucesso = false;
+  //pilhaBT.push(n);
+  //var fracasso = false, sucesso = false;
+  while((sucesso == false) && (fracasso == false)){
+        if(abertos.length == 0){                            ///Lista de abertos vazia
+            fracasso = true;
+        }
+        else{
+            var n = abertos[abertos.length - 1];                         ///Topo da pilha
+            if(limiteProfundidade > limiteProfMax){                     ///Estabelece um limite de profundidade da busca
+                limiteProfundidade--;
+                fechados.push(n);
+                abertos.pop();
+                //solucao.push_back(n);
+                continue;                                           ///Continua o loop sem descer o código
+            }
+            //fechados.push_back(n);
+            //abertos.pop_back();
+            //cout << n << endl;
+            //console.log("\n \n");
+            //console.log("Profundidade: " + limiteProfundidade);
+            //console.log("Abertos: " + limiteProfundidade);
+            /*cout << "Abertos: ";
+            for(int i = 0; i < abertos.size(); i++){
+                cout << abertos[i] << " -- ";
+            }
+            cout << endl;
+            cout << "Fechados: ";
+            for(int i = 0; i < fechados.size(); i++){
+                cout << fechados[i] << " -- ";
+            }
+            cout << endl << endl;*/
+
+            abertos.pop();
+            visitados[n.i][n.j] = true;                               //Marca como visitado o próximo vértice
+            if(n == fim){
+                sucesso = true;
+                fechados.push(n);
+                break;
+            }
+            else{
+                var contador = 0;
+                //var contVisitados = 3;
+                //var contVerticesNull = 0;
+                for (var i = s.wall.length - 1; i > -1; i--) {                  //Checa na direção contraria(esquerda,baixo,direita,cima) para poder retirar da pilha na ordem do vetor de paredes
+                  if(n.wall[i] === false){                                      //Checa se não há parede
+                    //console.log(i + " ---- "+ n.i + " ------(n) BACKTRACKING ---- " + n.j);
+                    var auxVizinho = n.getVizinho(i, this.matriz);                   //Captura a celula do vizinho na matriz
+                    //console.log(auxVizinho.i + " ------ BACKTRACKING ---- " + auxVizinho.j);
+                    if(visitados[auxVizinho.i][auxVizinho.j] === false){        //vizinho não visitado
+                      var u = auxVizinho;                                           //Atualiza para o próximo vértice
+                      abertos.push(u);
+                      //pilhaBT.push(n);
+                    }
+                  }
+                  else{
+                    //contVisitados++;
+                  }
+                  //contador++;
+                  contador++;
+              }
+              if(contador >= 4){ ///Passou todas as operações possíveis -- Fazer o backtracking senão retornar fracasso
+                limiteProfundidade++;
+                fechados.push(n);
+                solucao.push(n);
+              }
+            }
+        }
+    }
+
+    if(fracasso){
+      console.log("Fracasso em encontrar a solução");
+    }
+    else{
+      if(sucesso){
+        console.log("Sucesso em encontrar a solução");
+        var texto = "\nAbertos (total ao final da execucao: " + abertos.length + "): ";
+        for(var i = 0; i < abertos.length; i++){
+            texto = texto + "Celula["+abertos[i].i+"]["+abertos[i].j+"]"+ " -- ";
+        }
+        texto = texto + "\n";
+        console.log(texto);
+        texto = "";
+        var numVisitados = 0;
+        for (var i = 0; i < this.linhas; i++) {
+          for(var j = 0; j < this.colunas; j++){
+            if(visitados[i][j]){
+                texto = texto + "Celula["+i+"]["+j+"] -- ";
+                numVisitados++;
+            }
+          }
+        }
+        texto = "\n\nQuem foi visitado (total ao final da execucao: " + numVisitados + "): " + texto;
+        console.log(texto);
+        //console.log("\nQuantidade total de vértices visitados: " + numVisitados);
+        var tempoFinal = performance.now();
+        console.log("\nTempo de execução: " + parseFloat((tempoFinal - tempoInicial)).toFixed(3) + " milissegundos");    ///Mede o tempo somente na função
+      }
+    }
+
 }
